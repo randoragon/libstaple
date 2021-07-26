randoragon/libstaple

commit 8ad633dcb37fa57321e5767ae9ca20ff00e1a827

[stack] Fix get function and add get man page

src/rnd_stack.c
@@ -2435,9 +2435,9 @@ int rnd_stack_get(const struct rnd_stack *stack, size_t idx, void *output)
 		error(("output is NULL"));
 		return RND_EINVAL;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
-		return RND_EILLEGAL;
+	if (idx > stack->size) {
+		error(("index out of range"));
+		return RND_EINDEX;
 	}
 #endif
 	src = (char*)stack->data + (stack->size - 1 - idx) * stack->elem_size;
@@ -2457,8 +2457,8 @@ char rnd_stack_getc(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(char)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2477,8 +2477,8 @@ short rnd_stack_gets(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(short)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2497,8 +2497,8 @@ int rnd_stack_geti(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(int)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2517,8 +2517,8 @@ long rnd_stack_getl(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(long)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2537,8 +2537,8 @@ signed char rnd_stack_getsc(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(signed char)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2557,8 +2557,8 @@ unsigned char rnd_stack_getuc(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(unsigned char)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2577,8 +2577,8 @@ unsigned short rnd_stack_getus(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(unsigned short)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2597,8 +2597,8 @@ unsigned int rnd_stack_getui(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(unsigned int)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2617,8 +2617,8 @@ unsigned long rnd_stack_getul(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(unsigned long)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2637,8 +2637,8 @@ float rnd_stack_getf(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(float)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2657,8 +2657,8 @@ double rnd_stack_getd(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(double)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif
@@ -2677,8 +2677,8 @@ long double rnd_stack_getld(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(long double)));
 		return 0;
 	}
-	if (stack->size == 0) {
-		error(("stack is empty"));
+	if (idx > stack->size) {
+		error(("index out of range"));
 		return 0;
 	}
 #endif

man/rnd_stack_get.3
@@ -1,0 +1,174 @@
+.TH RND_STACK_GET 3 DATE "librnd-VERSION"
+.SH NAME
+rnd_stack_get - look up arbitrary element in a stack
+.SH SYNOPSIS
+.ad l
+#include <rnd.h>
+.sp
+int
+.BR rnd_stack_get "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx ,
+void
+.RI * output )
+.br
+char
+.BR rnd_stack_getc "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+short
+.BR rnd_stack_gets "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+int
+.BR rnd_stack_geti "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+long
+.BR rnd_stack_getl "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+signed char
+.BR rnd_stack_getsc "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+unsigned char
+.BR rnd_stack_getuc "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+unsigned short
+.BR rnd_stack_getus "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+unsigned int
+.BR rnd_stack_getui "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+unsigned long
+.BR rnd_stack_getul "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+float
+.BR rnd_stack_getf "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+double
+.BR rnd_stack_getd "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.br
+long double
+.BR rnd_stack_getld "(const struct rnd_stack"
+.RI * stack ,
+size_t
+.IR idx )
+.ad
+.SH DESCRIPTION
+.P
+The functions in the
+.BR rnd_stack_get ()
+family are used to look up the value of an arbitrary element in a stack. The
+looked up element remains on the stack.
+.P
+.I stack
+is the stack.
+.P
+.I idx
+is the index of the element to look up. Indices start from 0 and extend
+downwards \(em 0 is the top element, 1 is the element below it, and so on.
+.br
+.I idx
+must be smaller than
+.IR stack "'s " size .
+.SS Generic Form
+.I output
+is the start of an adequately-sized block of memory the element shall be copied
+into.
+.I output
+must not overlap in memory with the removed element.
+.SH RETURN VALUE
+.P
+If successful, the functions in the
+.BR rnd_stack_get ()
+family shall return 0. Otherwise, they shall return a code indicating the
+error.
+.SH ERRORS
+.SS Generic Form
+The
+.BR rnd_stack_get ()
+function shall fail if:
+.IP \fBRND_EINVAL\fP 1.5i
+Either
+.IR stack " or " output
+are null pointers (debug mode only).
+.IP \fBRND_EINDEX\fP 1.5i
+.I idx
+out of range (debug mode only).
+.SS Suffixed Form
+The suffixed
+.BR rnd_stack_get ()
+functions shall fail if:
+.IP 0 1.5i
+.I stack
+is a null pointer (debug mode only).
+.IP 0 1.5i
+.I idx
+out of range (debug mode only).
+.IP 0 1.5i
+.IR stack "'s " elem_size
+does not match the size of the function's return type (debug mode only).
+.P
+Note that 0 can also be a successful return value, since it is a number that can
+be stored on the stack, like any other.
+.SH NOTES
+For practical reasons, functions of the suffixed form do not provide an
+unambiguous error-detecting interface. This design choice was made because these
+functions can only fail if the user misuses the library.
+.P
+This means there is a guaranteed uncertainty whenever a suffixed function
+returns 0. However, failures can be told apart from successes depending on the
+presence or absence of an error message (quiet mode needs to be disabled).
+.SH CONFORMING TO
+See
+.BR librnd (7).
+.SH SEE ALSO
+.ad l
+.BR librnd (7),
+.BR rnd_stack (7),
+.BR rnd_stack_create (3),
+.BR rnd_stack_destroy (3),
+.BR rnd_stack_clear (3),
+.BR rnd_stack_copy (3),
+.BR rnd_stack_map (3),
+.BR rnd_stack_push (3),
+.BR rnd_stack_insert (3),
+.BR rnd_stack_quickinsert (3),
+.BR rnd_stack_peek (3),
+.BR rnd_stack_pop (3),
+.BR rnd_stack_remove (3),
+.BR rnd_stack_quickremove (3),
+.BR rnd_stack_set (3),
+.BR rnd_stack_print (3)
+

commit ec41b47d295506017743f36d2bde9935fa11f2fc

[stack] Fix up some error cases and the test unit
8ad633dc introduced some improper error checks (EINDEX in get/set
functions should be thrown when idx >= size, not idx > size). It also
changed the returned error code (EILLEGAL -> EINDEX). The culmination of
these two overlooks had caused the test unit to throw errors. All fixed
now.

In addition, I've removed some duplicate error checks from insert
functions.

src/rnd_stack.c
@@ -970,10 +970,6 @@ int rnd_stack_quickinsertsc(struct rnd_stack *stack, size_t idx, signed char ele
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
-	if (idx > stack->size) {
-		error(("index out of range"));
-		return RND_EINDEX;
-	}
 #endif
 	if (rnd_size_try_add(stack->size * stack->elem_size, stack->elem_size))
 		return RND_ERANGE;
@@ -1029,10 +1025,6 @@ int rnd_stack_quickinsertus(struct rnd_stack *stack, size_t idx, unsigned short 
 		error(("stack->elem_size is incompatible with elem type ("SIZE_T_FMT" != "SIZE_T_FMT")",
 					stack->elem_size, sizeof(elem)));
 		return RND_EILLEGAL;
-	}
-	if (idx > stack->size) {
-		error(("index out of range"));
-		return RND_EINDEX;
 	}
 	if (idx > stack->size) {
 		error(("index out of range"));
@@ -2435,7 +2427,7 @@ int rnd_stack_get(const struct rnd_stack *stack, size_t idx, void *output)
 		error(("output is NULL"));
 		return RND_EINVAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2457,7 +2449,7 @@ char rnd_stack_getc(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(char)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2477,7 +2469,7 @@ short rnd_stack_gets(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(short)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2497,7 +2489,7 @@ int rnd_stack_geti(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(int)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2517,7 +2509,7 @@ long rnd_stack_getl(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(long)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2537,7 +2529,7 @@ signed char rnd_stack_getsc(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(signed char)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2557,7 +2549,7 @@ unsigned char rnd_stack_getuc(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(unsigned char)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2577,7 +2569,7 @@ unsigned short rnd_stack_getus(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(unsigned short)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2597,7 +2589,7 @@ unsigned int rnd_stack_getui(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(unsigned int)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2617,7 +2609,7 @@ unsigned long rnd_stack_getul(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(unsigned long)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2637,7 +2629,7 @@ float rnd_stack_getf(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(float)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2657,7 +2649,7 @@ double rnd_stack_getd(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(double)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2677,7 +2669,7 @@ long double rnd_stack_getld(const struct rnd_stack *stack, size_t idx)
 					stack->elem_size, sizeof(long double)));
 		return 0;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return 0;
 	}
@@ -2698,7 +2690,7 @@ int rnd_stack_set(struct rnd_stack *stack, size_t idx, void *val)
 		error(("val is NULL"));
 		return RND_EINVAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2721,7 +2713,7 @@ int rnd_stack_setc(struct rnd_stack *stack, size_t idx, char val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2744,7 +2736,7 @@ int rnd_stack_sets(struct rnd_stack *stack, size_t idx, short val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2767,7 +2759,7 @@ int rnd_stack_seti(struct rnd_stack *stack, size_t idx, int val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2790,7 +2782,7 @@ int rnd_stack_setl(struct rnd_stack *stack, size_t idx, long val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2813,11 +2805,11 @@ int rnd_stack_setsc(struct rnd_stack *stack, size_t idx, signed char val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2840,7 +2832,7 @@ int rnd_stack_setuc(struct rnd_stack *stack, size_t idx, unsigned char val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2863,11 +2855,11 @@ int rnd_stack_setus(struct rnd_stack *stack, size_t idx, unsigned short val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2890,7 +2882,7 @@ int rnd_stack_setui(struct rnd_stack *stack, size_t idx, unsigned int val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2913,7 +2905,7 @@ int rnd_stack_setul(struct rnd_stack *stack, size_t idx, unsigned long val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2936,7 +2928,7 @@ int rnd_stack_setf(struct rnd_stack *stack, size_t idx, float val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2959,7 +2951,7 @@ int rnd_stack_setd(struct rnd_stack *stack, size_t idx, double val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}
@@ -2982,7 +2974,7 @@ int rnd_stack_setld(struct rnd_stack *stack, size_t idx, long double val)
 					stack->elem_size, sizeof(val)));
 		return RND_EILLEGAL;
 	}
-	if (idx > stack->size) {
+	if (idx >= stack->size) {
 		error(("index out of range"));
 		return RND_EINDEX;
 	}

test/src/stack.c
@@ -268,7 +268,7 @@ TEST get_set(void)
 		ASSERT_EQ_FMT(b, out, "%f");
 		ASSERT_EQ_FMT(0, rnd_stack_pop(s, &out), "%d");
 		ASSERT_EQ_FMT(b, out, "%f");
-		ASSERT_EQ_FMT(RND_EILLEGAL, rnd_stack_get(s, 0, &out), "%d");
+		ASSERT_EQ_FMT(RND_EINDEX, rnd_stack_get(s, 0, &out), "%d");
 		ASSERT_EQ_FMT(0, rnd_stack_destroy(s, NULL), "%d");
 	}
 