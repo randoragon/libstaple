d645f95efb40046452d3f190a59b2714a335474d
Randoragon <randoragongamedev@gmail.com>
2023-06-17 12:59:05 +0200

    ci(entrypoint): Speed up make with -j flag

--- test/docker-entrypoint.sh
+++ test/docker-entrypoint.sh
@@ -20,7 +20,7 @@ run_tests () {
 
     # For some reason valgrind crashes inside the docker image, disable it.
     # Hopefully I can find a fix for this later, memory safety is important.
-    make LUA=lua5.3 CC="$1" VALGRIND= clean test_clean test
+    make -j$(nproc) LUA=lua5.3 CC="$1" VALGRIND= clean test_clean test
 }
 run_tests gcc
 run_tests clang
