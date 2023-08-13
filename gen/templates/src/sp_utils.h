e91f218b809ed09c9a7ee197ec60fd7d3dc485b1
Randoragon <randoragongamedev@gmail.com>
2023-08-13 14:06:50 +0200

    feat(utils): Add boolean macros
    For SP_SIZEOF_BOOL I could ask the user to just write 0, but this is
    more readable in-code, and makes it possible to alter the API without
    breaking user code in the future (although I highly, *highly* doubt it
    will happen).
    
    SP_BYTE_SIZE has similar rationale, I'm not yet sure if keeping the code
    generalized for an arbitrary CHAR_BIT will be feasible, so if worst
    comes to worst, I can just change it to 8 in a single place. And if it
    will be feasible, it's nice to expose SP_BYTE_SIZE in case the user
    wants to know for some secret sauce optimization.
    
    Issue: #6

--- gen/templates/src/sp_utils.h
+++ gen/templates/src/sp_utils.h
@@ -3,6 +3,11 @@
  * do not belong in just one module or data structure.
  */
 /*H}*/
+#include <limits.h>
+
+/* Used for data structures with binary element size */
+#define SP_BYTE_SIZE CHAR_BIT
+#define SP_SIZEOF_BOOL 0
 
 int sp_is_debug(void);
 int sp_is_quiet(void);
--- src/sp_utils.h
+++ src/sp_utils.h
@@ -21,6 +21,11 @@
 /* This header defines functions that should be exposed to the library user, but
  * do not belong in just one module or data structure.
  */
+#include <limits.h>
+
+/* Used for data structures with binary element size */
+#define SP_BYTE_SIZE CHAR_BIT
+#define SP_SIZEOF_BOOL 0
 
 int sp_is_debug(void);
 int sp_is_quiet(void);
